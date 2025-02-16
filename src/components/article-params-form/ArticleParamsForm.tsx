9e51d04d0fef17a464e24471b29ba6869a1f34d8
Irina Ignateva <ignatyeva_irina@inbox.ru>
2025-02-16 17:34:34 +0300

    1. open sidebar

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,52 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleIsOpen = () => {
+		setIsOpen((currentValue) => !currentValue);
+	};
+
+	const paramsPanel = useRef<HTMLFormElement | null>(null);
+	const nonFirstClick = useRef<boolean | null>(false)
+
+	useEffect(() => {
+		const handleOutsideClick = (evt: Event) => {
+			if (
+				evt.target instanceof Node &&
+				!paramsPanel.current?.contains(evt.target) &&
+				nonFirstClick.current
+			) {
+				setIsOpen(false);
+			}
+			nonFirstClick.current = true;
+			evt.stopPropagation();
+		};
+
+		if (isOpen) {
+			nonFirstClick.current = false;
+			window.addEventListener('click', handleOutsideClick);
+		}
+
+		return () => {
+			 window.removeEventListener('click', handleOutsideClick);
+		}
+	});
+
+	const containerClasses = clsx({
+		[styles.container]: true,
+		[styles.container_open]: isOpen,
+	});
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={toggleIsOpen} />
+			<aside className={containerClasses}>
+				<form ref={paramsPanel} className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
